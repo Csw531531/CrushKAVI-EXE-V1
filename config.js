704f7da66cc32e36d78cb61422e9fa844349ca83
Csw531531 <senuralakshan456@gmail.com>
2025-03-07 19:36:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=blggXARQ#eUOQKJt-oP-IRfMGi_FAZEO3zU3BUa2KdgIB2SjKIrw",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=fshAFDYB#RkgIDeWXS49bqEre5b7_uyVzVKkPIfPYQ99OR1UIEvM",
 MONGODB: process.env.MONGODB|| "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya",
 };
